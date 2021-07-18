db5887c31d20db3563dbd30d31b28d6bbeaf9dae
EternalTKD <75866051+EternalTKD@users.noreply.github.com>
2021-07-17 19:43:09 -0500

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -4,7 +4,7 @@ import random
 from cogs.moderation import staff_list
 import discord
 from discord.ext import commands
-
+map(x, staff_list)
 
 class Misc(commands.Cog):
     def __init__(self, bot):
@@ -88,7 +88,7 @@ class Misc(commands.Cog):
     @commands.command(name="staff", description="List of staff members", aliases=["staff_members", "staff_list"])
     async def staff(self, ctx):
         
-        embed=discord.Embed(title='Staff Members', description=f'<@{str("/n".join(staff_list))}>', color=0x03D5D6)
+        embed=discord.Embed(title='Staff Members', description=f'<@{str("/n".join(x))}>', color=0x03D5D6)
         await ctx.send(embed=embed)
 
 def setup(bot):
