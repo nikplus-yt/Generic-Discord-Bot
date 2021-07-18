ac61fbbb07f92aebd3146494c58a05e7f5abe0e9
EternalTKD <75866051+EternalTKD@users.noreply.github.com>
2021-07-17 20:00:48 -0500

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -87,9 +87,8 @@ class Misc(commands.Cog):
             
     @commands.command(name="staff", description="List of staff members", aliases=["staff_members", "staff_list"])
     async def staff(self, ctx):
-        
-        embed=discord.Embed(title='Staff Members', description=f"{"\n".join([str(i) for i in staff_list]}", color=0x03D5D6)
-        await ctx.send(embed=embed)
+        #embed=discord.Embed(title='Staff Members', description=f"{"\n".join([str(i) for i in staff_list]}", color=0x03D5D6)
+        await ctx.send("\n".join([str(i) for i in staff_list]))
 
 def setup(bot):
     bot.add_cog(Misc(bot))
