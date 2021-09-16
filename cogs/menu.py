643274d2ba5f1ea73f8fc6072d75a2220efd57e2
EternalTKD <75866051+EternalTKD@users.noreply.github.com>
2021-09-16 16:51:45 -0500

    Add files via upload

---
+++ cogs/menu.py
@@ -0,0 +1,25 @@
+import discord
+import DiscordUtils
+from discord.ext import commands
+import discord_components import *
+
+class Menus(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+
+    @commands.command()
+    async def menu1(self, ctx):
+        await ctx.send("Just select the roles you're interested in below.", components = [Select(placeholder="Make a selection"), options=[SelectOption(label="Dead Chat Ping", value='A')SelectOption(label="test", value='B')]])
+        interaction = await self.bot.wait_for("select_option", check = lambda i: i.component[0].value == "A")
+        await interaction.respond(content = f"You have selected `{interaction.component[0].label}`")
+
+
+
+
+
+
+        
+def setup(bot):
+    bot.add_cog(Menus(bot))
